3a267e6e79dab2cb46321475f7ace7af08f0a2c3
Shr1mp <1205874457@qq.com>
2024-09-15 11:09:34 +0800

    添加lombok依赖和Tank父类

---
+++ src/main/java/game/tankwar/entity/Tank.java
@@ -0,0 +1,13 @@
+package game.tankwar.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tank {
+    private int x_location;
+    private int y_location;
+}
